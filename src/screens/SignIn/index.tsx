5ac366c1ce12cbe313aea51667bb3dfe78b72eb3
BrunoLagoa <miroldols@gmail.com>
2021-08-09 20:52:35 -0300

    Pesistindo dados no watermelondb

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   StatusBar,
   KeyboardAvoidingView,
@@ -15,8 +15,6 @@ import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
 
 import { useAuth } from '../../hooks/auth';
-
-import { database } from '../../database';
 
 import { Container, Header, Title, SubTitle, Form, Footer } from './styles';
 
@@ -56,16 +54,6 @@ export function SignIn() {
   function handleNewAccount() {
     navigation.navigate('SignUpFirstStep');
   }
-
-  useEffect(() => {
-    async function loadData() {
-      const userCollection = database.get('users');
-      const users = await userCollection.query().fetch();
-      console.log(users);
-    }
-
-    loadData();
-  }, []);
 
   return (
     <KeyboardAvoidingView behavior='position' enabled>
