a234c83c116cbf730b051b9c3a65a25feaed3347
BrunoLagoa <miroldols@gmail.com>
2021-08-10 01:43:12 -0300

    Criando funcao de signOut

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -8,6 +8,7 @@ import { useNavigation } from '@react-navigation/core';
 import { useTheme } from 'styled-components';
 import { Feather } from '@expo/vector-icons';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import * as ImagePicker from 'expo-image-picker';
 
 import { BackButton } from '../../components/BackButton';
 import { Input } from '../../components/Input';
@@ -31,10 +32,21 @@ import {
   Section,
 } from './styles';
 
+interface ImagePickerProps {
+  cancelled: boolean;
+  height: number;
+  width: number;
+  uri: string;
+}
+
 export function Profile() {
+  const { user, signOut } = useAuth();
   const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
 
-  const { user } = useAuth();
+  const [avatar, setAvatar] = useState(user.avatar);
+  const [name, setName] = useState(user.name);
+  const [driverLicence, setDriverLicence] = useState(user.driver_license);
+
   const theme = useTheme();
   const navigation = useNavigation();
 
@@ -42,10 +54,23 @@ export function Profile() {
     navigation.goBack();
   }
 
-  function handleSinOut() {}
-
   function handleOptionChange(optionSelected: 'dataEdit' | 'passwordEdit') {
     setOption(optionSelected);
+  }
+
+  async function handleAvatarSelect() {
+    const result = (await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    })) as ImagePickerProps;
+
+    if (result.cancelled) return;
+
+    if (result.uri) {
+      setAvatar(result.uri);
+    }
   }
 
   return (
@@ -56,14 +81,14 @@ export function Profile() {
             <HeaderTop>
               <BackButton color={theme.colors.shape} onPress={handleBack} />
               <HeaderTitle>Editar Perfil</HeaderTitle>
-              <LogoutButton onPress={handleSinOut}>
+              <LogoutButton onPress={signOut}>
                 <Feather name='power' size={24} color={theme.colors.shape} />
               </LogoutButton>
             </HeaderTop>
 
             <PhotoContainer>
-              <Photo source={{ uri: 'https://github.com/brunolagoa.png' }} />
-              <PhotoButton onPress={() => {}}>
+              {!!avatar && <Photo source={{ uri: avatar }} />}
+              <PhotoButton onPress={handleAvatarSelect}>
                 <Feather name='camera' size={24} color={theme.colors.shape} />
               </PhotoButton>
             </PhotoContainer>
@@ -94,6 +119,7 @@ export function Profile() {
                   placeholder='Nome'
                   autoCorrect={false}
                   defaultValue={user.name}
+                  onChangeText={setName}
                 />
                 <Input
                   iconName='mail'
@@ -105,6 +131,7 @@ export function Profile() {
                   placeholder='CNH'
                   keyboardType='numeric'
                   defaultValue={user.driver_license}
+                  onChangeText={setDriverLicence}
                 />
               </Section>
             ) : (
